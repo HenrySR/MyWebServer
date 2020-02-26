c7bb0d54c6f7191fc000c5eb5f801f13ce9abc52
Mollie Shichman <mfshichman@email.wm.edu>
2020-02-26 16:28:32 -0500

    bare bones code from slides

--- MyWebServer.java
+++ MyWebServer.java
@@ -1,1 +1,24 @@
-Java.
+import java.io.*;
+import java.net.*; 
+
+class MyWebServer{
+    public static void main(String[] args){
+        int port = Integer.parseInt(args[0]);
+        String fileDir = args[1];
+        
+        String clientSentence;
+        String capitalizedSentence; 
+        try{
+            ServerSocket welcomeSocket = new ServerSocket(port);
+            while(true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                outToClient.writeBytes(fileDir);
+                connectionSocket.close();
+            }
+        } catch (IOException e){
+            System.out.println(e);
+
+    }
+}
