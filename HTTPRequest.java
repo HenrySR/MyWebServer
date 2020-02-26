41283369f9cb150e7c68acf1a76e6395615d6b1f
Mollie Shichman <mfshichman@email.wm.edu>
2020-02-26 16:43:56 -0500

    step 1 done & httprequest bare bones

---
+++ HTTPRequest.java
@@ -0,0 +1,6 @@
+
+class HTTPRequest{
+    public HTTPRequest(){
+
+    }
+}
--- MyWebServer.java
+++ MyWebServer.java
@@ -6,19 +6,21 @@ class MyWebServer{
         int port = Integer.parseInt(args[0]);
         String fileDir = args[1];
         
-        String clientSentence;
-        String capitalizedSentence; 
+        // String clientSentence;
+        // String capitalizedSentence; 
         try{
             ServerSocket welcomeSocket = new ServerSocket(port);
             while(true) {
                 Socket connectionSocket = welcomeSocket.accept();
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
                 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-                outToClient.writeBytes(fileDir);
+                String test = inFromClient.readLine();
+                outToClient.writeBytes(test);
                 connectionSocket.close();
+                System.out.println("It works!");
             }
         } catch (IOException e){
             System.out.println(e);
-
+        }
     }
 }
